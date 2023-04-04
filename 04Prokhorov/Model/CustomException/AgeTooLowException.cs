4743ff9d984e8a806a5a1522cbcd5343589ead7c
Oleksandr Prokhorov o.prokhorov@pinglestudio.com <o.prokhorov@pinglestudio.com>
2023-04-04 23:00:45 +0300

    Add project files.

---
+++ 04Prokhorov/Model/CustomException/AgeTooLowException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _04Prokhorov.Model.CustomException
+{
+    internal class AgeTooLowException : Exception
+    {
+        public AgeTooLowException(string message) : base(message)
+        {
+            MessageBox.Show("didn't born");
+        }
+
+
+    }
+}
---
+++ 04Prokhorov/Model/CustomException/WrongEmailException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _04Prokhorov.Model.CustomException
+{
+    internal class WrongEmailException : Exception
+    {
+        public WrongEmailException(string message) : base(message)
+        {
+            MessageBox.Show("Wrong Email");
+        }
+    }
+}
